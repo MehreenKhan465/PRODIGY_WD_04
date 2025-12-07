4b3c205d0b319a491b7a3674e039f7437bad4386
MehreenKhan465 <33mehreenjkhan@gmail.com>
2025-12-07 20:39:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Navbar color change on scroll
+window.addEventListener("scroll", function () {
+    const nav = document.getElementById("navbar");
+    if (window.scrollY > 50) {
+        nav.classList.add("scrolled");
+    } else {
+        nav.classList.remove("scrolled");
+    }
+});
